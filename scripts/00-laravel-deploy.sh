77297d5aff4f4bba76c42d8c1b1582c96373d06a
Aeo Zatoichi Bax <109118450+im-aeo@users.noreply.github.com>
2024-02-21 13:43:35 -0500

    Update 00-laravel-deploy.sh

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -4,6 +4,11 @@ echo "Running composer"
 composer global require hirak/prestissimo
 composer install --no-dev --working-dir=/var/www/html
 
+echo "Clearing old caches..."
+php artisan cache:clear
+php artisan cache:clear --store=redis
+npm cache clean --force command
+
 echo "Caching config..."
 php artisan config:cache
 
@@ -13,8 +18,8 @@ php artisan route:cache
 echo "Running migrations..."
 php artisan migrate --force
 
-#echo "Running seeders..."
-#php artisan db:seed
+echo "Running seeders..."
+php artisan db:seed
 
 echo "Running vite..."
 npm install
