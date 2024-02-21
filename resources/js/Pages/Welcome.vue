bb963c58e43c808234539d14d5f1df6a6bb10566
Aeo Zatoichi Bax <109118450+im-aeo@users.noreply.github.com>
2024-02-21 10:25:55 -0500

    Update Welcome.vue

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -11,12 +11,13 @@ const site = computed(() => usePage<any>().props.site)
 
 defineProps({
     UserCount: { type: Number, default: '0' },
+    LandingItems: { type: Array },
 });
 </script>
 <style scoped src="@/Pages/landing.css"></style>
 <template>
     <Navbar>
-    <header class="masthead-landing bg-info">
+        <header class="masthead-landing bg-info">
         <div class="cell">
             <div class="justify-center row-landing align-center">
                 <div class="row-landing justify-content-center">
@@ -24,9 +25,14 @@ defineProps({
                         <div class="text-center text-white">
                             <!-- Page heading-->
                             <h1 class="mb-5"><v-lazy-image :src="site.logo" width="512" /></h1>
-                            <h3 class="mt-3  fw-semibold">Join in on the action today. Buy &amp; sell items, participate
+                            <h3 class="mt-3 fw-semibold">Join in on the action today. Buy &amp; sell items, participate
                                 in groups, make friends, and more.</h3>
-                            <h4 class="fw-semibold">Join <strong>{{ UserCount }}</strong> {{ UserCount != "1" ? 'users' : 'user' }} today!</h4>
+                            <div class="min-w-0 gap-4 mt-3 flex-container align-center">
+                                <button :href="route('auth.login.page')" class=" btn btn-landing btn-warning"><i
+                                        class="mr-1 fas fa-key"></i> Login</button>
+                                <button :href="route('auth.register.page')" class="btn btn-landing btn-success"><i
+                                        class="mr-1 fas fa-user-plus"></i> Register</button>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -36,94 +42,133 @@ defineProps({
     <div class="container site-landing grid-x">
         <div>
             <div class="row-landing" id="YASQUEEN">
-                <div class="col-md-6-landing">
-                    <div class="card">
-                        <div class="card-body">
-                            <div class="row-landing align-items-center-landing">
-                                <div class="text-center col-md-3-landing text-center-sm">
-                                    <i class="fas fa-user-hard-hat text-info hide-sm-landing" style="font-size:90px;"></i>
-                                    <i class="mb-2 fas fa-user-hard-hat text-info show-sm-only-landing" style="font-size:80px;"></i>
-                                </div>
-                                <div class="col-md-9-landing">
-                                    <div class="landing-text-landing-landing fw-semibold">Customize Your Character</div>
-                                    <div>Pick and choose from a collection of items created by the community to create
-                                        your own unique character with.</div>
+                <div class="col-md-12-landing">
+                    <div class="row-landing">
+                        <div class="text-right col-md-4-landing">
+                            <div class="landing-text-landing fw-semibold">Customize your Character</div>
+                            <div>Pick and choose from a collection of items created by the community to create you own
+                                unique character with.</div>
+                        </div>
+                        
+                        <div class="col-md-2-landing" v-for="item in LandingItems">
+                            <div class="cell large-2 medium-3 small-6">
+                                <a href="#" class="d-block">
+                                    <div class="card position-relative p-2 mb-1">
+                                        <img src="item.thumbnail" />
+                                    </div>
+                                    <div class="text-body fw-semibold text-truncate">
+                                        {{ item.name }}
+                                    </div>
+                                </a>
+                                <div class="text-xs fw-semibold">
+                                    <span class="text-muted">By</span>
+                                    <a href="#" class="text-danger">{{ '@' + item.creator.username }}<i
+                                            class="fas fa-shield-check text-success ms-1"></i></a>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                </div>
-                <div class="col-md-6-landing">
-                    <div class="card">
-                        <div class="card-body">
-                            <div class="row-landing align-items-center-landing">
-                                <div class="text-center col-md-3-landing text-center-sm show-sm-only-landing">
-                                    <i class="mb-2 fas fa-users text-success" style="font-size:80px;"></i>
-                                </div>
-                                <div class="text-right col-md-8-landing">
-                                    <div class="landing-text-landing fw-semibold">Meet New Friends</div>
-                                    <div>On {{ site.name }}, finding and adding friends is only two steps away,
-                                        simply find a
-                                        user and add them. It's that simple!</div>
-                                </div>
-                                <div class="text-center col-md-4-landing text-center-sm hide-sm-landing">
-                                    <i class="fas fa-users text-success" style="font-size:90px;"></i>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-6-landing">
-                    <div class="card">
-                        <div class="card-body">
-                            <div class="row-landing align-items-center-landing">
-                                <div class="text-center col-md-3-landing text-center-sm">
-                                    <i class="fas fa-tools text-danger hide-sm-landing" style="font-size:90px;"></i>
-                                    <i class="mb-2 fas fa-tools text-danger show-sm-only-landing" style="font-size:80px;"></i>
-                                </div>
-                                <div class="col-md-9-landing">
-                                    <div class="landing-text-landing fw-semibold">Create Your Own Assets</div>
-                                    <div>Unleash your inner entrepreneur by creating and marketing your own items,
-                                        groups, games and more.</div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-6-landing">
-                    <div class="card">
-                        <div class="card-body">
-                            <div class="row-landing align-items-center-landing">
-                                <div class="text-center col-md-3-landing text-center-sm show-sm-only-landing">
-                                    <i class="mb-2 fas fa-comments-alt text-warning" style="font-size:80px;"></i>
-                                </div>
-                                <div class="text-right col-md-8-landing">
-                                    <div class="landing-text-landing fw-semibold">Socialize With Others</div>
-                                    <div>Head over to Discussion to socialize with all kinds of users on {{ site.name }}, from
-                                        the funny to the professional, and find new friends!</div>
-                                </div>
-                                <div class="text-center col-md-4-landing text-center-sm hide-sm-landing">
-                                    <i class="fas fa-comments-alt text-warning" style="font-size:90px;"></i>
-                                </div>
-                            </div>
-                        </div>
+
                     </div>
                 </div>
             </div>
-        </div>
+
+            <div class="row-landing mt-4 mb-4" id="YASQUEEN">
+                <div class="col-md-12-landing">
+                    <div class="row-landing">
+                        <div class="col-md-4-landing mb-2">
+                            <div class="cell large-2 medium-3 small-6 mb-2">
+                                <div class="card card-post card-body section-borderless">
+                                    <div class="section">
+                                        <div class="flex-container align-justify align-middle gap-2">
+                                            <div class="flex-container align-middle gap-2">
+                                                <i class="fa-solid fa-messages text-2xl text-warning"></i>
+                                                <div class="w-100">
+                                                    <div class="fw-semibold">Nabrious</div>
+                                                    <div class="text-sm  fw-semibold text-muted">
+                                                        Nabrious<span class="mx-1">&bullet;</span>35m ago
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <a href="#" class="btn btn-info btn-sm text-truncate min-w-0">View
+                                                Post</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="cell large-2 medium-3 small-6 mb-2">
+                                <div class="card card-post card-body section-borderless">
+                                    <div class="section">
+                                        <div class="flex-container align-justify align-middle gap-2">
+                                            <div class="flex-container align-middle gap-2">
+                                                <i class="fa-solid fa-messages text-2xl text-warning"></i>
+                                                <div class="w-100">
+                                                    <div class="fw-semibold">Nabrious</div>
+                                                    <div class="text-sm  fw-semibold text-muted">
+                                                        Nabrious<span class="mx-1">&bullet;</span>35m ago
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <a href="#" class="btn btn-info btn-sm text-truncate min-w-0">View
+                                                Post</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-md-4-landing">
+                            <div class="cell large-4 medium-3 small-6 mb-2">
+                                <div class="card card-post card-body section-borderless">
+                                    <div class="section">
+                                        <div class="flex-container align-justify align-middle gap-2">
+                                            <div class="flex-container align-middle gap-2">
+                                                <i class="fa-solid fa-messages text-2xl text-warning"></i>
+                                                <div class="w-100">
+                                                    <div class="fw-semibold">Nabrious</div>
+                                                    <div class="text-sm  fw-semibold text-muted">
+                                                        Nabrious<span class="mx-1">&bullet;</span>35m ago
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <a href="#" class="btn btn-info btn-sm text-truncate min-w-0">View
+                                                Post</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="cell large-4 medium-3 small-6 mb-2">
+                                <div class="card card-post card-body section-borderless">
+                                    <div class="section">
+                                        <div class="flex-container align-justify align-middle gap-2">
+                                            <div class="flex-container align-middle gap-2">
+                                                <i class="fa-solid fa-messages text-2xl text-warning"></i>
+                                                <div class="w-100">
+                                                    <div class="fw-semibold">Nabrious</div>
+                                                    <div class="text-sm  fw-semibold text-muted">
+                                                        Nabrious<span class="mx-1">&bullet;</span>35m ago
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <a href="#" class="btn btn-info btn-sm text-truncate min-w-0">View
+                                                Post</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="text-right col-md-4-landing">
+                            <div class="landing-text-landing fw-semibold">Socialize With Others</div>
+                            <div>Head over to Discussion to socialize with all kinds of users on {{ site.name }}, from
+                                the funny to the professional, and find new friends!</div>
+                        </div>
+                    </div>
+                  </div>
+              </div>
+          </div>
+       </div>
     </div>
-    <div class="justify-center text-center card card-body align-center reg-card">
-        <div class="col-md-8 align-center text-center-sm">
-            <h3>What are you wating for?</h3>
-            <p>Register to {{ site.name }} below.</p>
-            <div class="min-w-0 gap-1 mt-3 flex-container align-center">
-                <Link :href="route('auth.login.page')" class="gap-2 btn btn-warning"><i
-                    class="mr-1 fas fa-key"></i> Login</Link>
-                <Link :href="route('auth.register.page')" class="gap-2 btn btn-success"><i
-                    class="mr-1 fas fa-user-plus"></i> Register</Link>
-            </div>
-        </div>
-    </div>
-    <Footer/>
+</div>
+<Footer/>
 </Navbar>
 </template>
