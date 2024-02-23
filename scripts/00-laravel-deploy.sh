abd4cbea03602596401c672cbfe57ef00a9e045e
Aeo Zatoichi Bax <109118450+im-aeo@users.noreply.github.com>
2024-02-22 21:28:13 -0500

    Update 00-laravel-deploy.sh

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -21,5 +21,5 @@ echo "Running seeders..."
 php artisan db:seed
 
 echo "Running vite..."
-npm i --no-fund --prefer-offline --no-audit --progress=false --omit=optional --omit=dev
+npm i --progress=false
 npm run build
