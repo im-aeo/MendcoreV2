43a5f90eacbd5070c954b78dfb8a77342bc572c3
Aeo Zatoichi Bax <109118450+im-aeo@users.noreply.github.com>
2024-03-13 14:53:50 -0400

    Update 00-laravel-deploy.sh

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 echo "Installing Packages"
+npm install -g bun 
 composer install --no-dev --prefer-dist --no-interaction --no-autoloader --no-scripts -o --working-dir=/var/www/html
 composer dump-autoload --optimize
 
