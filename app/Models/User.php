b72a62a18679f4ee0afa5699f375f402c539a057
Aeo Zatoichi Bax <109118450+im-aeo@users.noreply.github.com>
2024-03-15 13:36:49 -0400

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,21 +2,22 @@
 
 namespace App\Models;
 
-use App\Models\Status;
-use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as AeoAuthenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 use App\Models\Avatar;
 use App\Models\Admin;
 use App\Models\Level;
+use App\Models\Status;
 use App\Models\ForumThread;
+use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use LevelUp\Experience\Concerns\GiveExperience;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as AeoAuthenticatable;
 
 class User extends AeoAuthenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, GiveExperience, Notifiable;
 
     /**
      * The attributes that are mass assignable.
