5b817ebb0831f643e0966b5bbdee46dd9abc99a6
Aeo Zatoichi Bax <109118450+im-aeo@users.noreply.github.com>
2024-03-18 14:48:57 -0400

    Update and rename LevelsSeeder.php to LevelSeeder.php

--- database/seeders/LevelSeeder.php
+++ database/seeders/LevelSeeder.php
@@ -1,10 +1,12 @@
 <?php
 
+namespace Database\Seeders;
+
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
-class LevelsSeeder extends Seeder
+class LevelSeeder extends Seeder
 {
     /**
      * Run the database seeds.
