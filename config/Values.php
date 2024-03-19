26142a50b8bdb739f4462fc7f30bf7a148a5ca9e
Aeo Zatoichi Bax <109118450+im-aeo@users.noreply.github.com>
2024-03-19 14:43:07 -0400

    Update Values.php

--- config/Values.php
+++ config/Values.php
@@ -6,16 +6,9 @@ return [
     'icon' => '/assets/img/logo.svg',
     'in_event' => true,
     'system_account_id' => 1,
-    'render' => [
-        'url' => '206.189.195.33:8001',
-        'default_avatar' => 'assets/img/dummy_headshot'
-    ],
+    
     'price' => [
         'username' => '250',
-    ],
-    'storage' => [
-        'root' => '/var/www/cdn',
-        'url' => 'https://cdn.netisu.com', // If your developing locally put localhost or your current server ip
     ],
     'production' => [
         'domains' => [
@@ -24,13 +17,9 @@ return [
             'careers' => 'https://careers.netisu.test',
         ],
     ],
+    
     'shop' => [
         'previews_enabled' => true
-    ],
-    'maintenance_passwords' => [
-        'randopass',
-        'password2',
-        'alexplaysminecraft123'
     ],
     'socials' => [
         'discord' => 'https://discord.gg/netisu',
