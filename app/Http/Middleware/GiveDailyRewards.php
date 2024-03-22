7ec785f76428ad3c25400535266bbdd5afff2109
Aeo Zatoichi Bax <109118450+im-aeo@users.noreply.github.com>
2024-03-22 11:06:00 -0400

    Update and rename GiveDailyCurrency.php to GiveDailyRewards.php

--- app/Http/Middleware/GiveDailyRewards.php
+++ app/Http/Middleware/GiveDailyRewards.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Http;
 
-class GiveDailyCurrency
+class GiveDailyRewards
 {
     /**
      * Handle an incoming request.
