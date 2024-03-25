d5c5258eac710cd9176c7018831df31004f75759
Aeo Zatoichi Bax <109118450+im-aeo@users.noreply.github.com>
2024-03-25 08:53:41 -0400

    Update AdminController.php

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -1,6 +1,18 @@
 <?php
+namespace App\Http\Controllers;
 
-
+use Inertia\Inertia;
+use App\Models\User;
+use App\Models\Item;
+use Inertia\Response;
+use App\Models\Avatar;
+use App\Models\UserSettings;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rules;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\RedirectResponse;
 
 class AdminController extends Controller {
   public function PanelIndex() {
