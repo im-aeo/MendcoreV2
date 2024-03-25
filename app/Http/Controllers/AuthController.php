32655acc2435782e4adf92f9788bfd628d173d7e
Aeo Zatoichi Bax <109118450+im-aeo@users.noreply.github.com>
2024-03-25 15:23:15 -0400

    Update AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,24 +2,20 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
 use App\Models\User;
+use Inertia\Inertia;
+use App\Models\Avatar;
 use App\Models\UserSettings;
-//use App\Http\Requests\Auth\LoginRequest;
-use App\Providers\RouteServiceProvider;
-use Illuminate\Auth\Events\Registered;
-use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rules;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Validation\Rules;
-use Inertia\Inertia;
-use Inertia\Response;
-use Illuminate\Support\Facades\Redirect;
-use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Auth\Events\Registered;
+use App\Providers\RouteServiceProvider;
 use Illuminate\Validation\ValidationException;
-use Predis\Client;
-use App\Models\Avatar;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
 class AuthController extends Controller
 {
@@ -138,7 +134,6 @@ class AuthController extends Controller
         // Create associated avatar
         Avatar::create([
             'user_id' => $user->id,
-            // Add other avatar fields here
         ]);
     
         event(new Registered($user));
