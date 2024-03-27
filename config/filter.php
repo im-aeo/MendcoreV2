98c4a0ac3d0621e2e898a397989f0f1e8b5abaa1
Aeo Zatoichi Bax <109118450+im-aeo@users.noreply.github.com>
2024-03-26 21:05:49 -0400

    Create filter.php

---
+++ config/filter.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+  'blacklist' => [
+    // List of blacklisted words here
+  ],
+  'whitelist_domains' => [
+    // List of allowed domains here (e.g., 'your-domain.com', 'subdomain.your-domain.com')
+  ],
+];
+
