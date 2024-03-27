35e5a0cffd7388004e466d0c619c45a4be63976c
Aeo Zatoichi Bax <109118450+im-aeo@users.noreply.github.com>
2024-03-26 21:07:05 -0400

    Update filter.php

--- config/filter.php
+++ config/filter.php
@@ -2,10 +2,15 @@
 
 return [
   'blacklist' => [
-    // List of blacklisted words here
+    // Your list of blacklisted words here
   ],
   'whitelist_domains' => [
-    // List of allowed domains here (e.g., 'your-domain.com', 'subdomain.your-domain.com')
+    // Your list of allowed domains here (e.g., 'yourdomain.com')
+  ],
+  'routes' => [
+    // ... Your route mappings with filterable fields
+  ],
+  'api_urls' => [
+    // ... Your API URL mappings with filterable fields
   ],
 ];
-
